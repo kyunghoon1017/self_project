4349d19febab17f864811b7dfa18726a85745e39
정경훈 <87818170+kyunghoon1017@users.noreply.github.com>
2022-02-23 17:14:21 +0900

    [Add] 메인 브랜치 생성 및 작업진행 중

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,6 +5,12 @@ const Main = () => {
   return (
     <div>
       <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
     </div>
   );
 };
